74d5ffbb942c32fb5594fb654e22c95eb18b9e97
dima999572 <dima999572.dubinevich@gmail.com>
2025-04-11 22:10:02 +0200

    Delete unnecessary files from the outdated configuration

--- terraform_deployment/acl_policy.tf
+++
@@ -1,9 +0,0 @@
-resource "vault_policy" "artifactory_policy" {
-  name = "artifactory"
-
-  policy = <<EOT
-path "artifactory/token/viewer" {
-    capabilities = ["read"]
-}
-EOT
-}
--- terraform_deployment/locals.tf
+++
@@ -1,6 +0,0 @@
-locals {
-  integration_vault_server = "http://127.0.0.1:8200"
-  integration_namespace    = "vault"
-
-  jfrog_server = "http:127.0.0.1:8082"
-}
